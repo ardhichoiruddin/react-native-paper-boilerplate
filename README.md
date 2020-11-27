0249477a483efe36959f7fd12cf5f1084fdc2051
ardhichoiruddin <ardhichoiruddin@yandex.com>
2020-11-27 15:42:51 +0700

    Add osmixCSX and fonts style

--- README.md
+++ README.md
@@ -5,5 +5,9 @@ This is boilerplate React-native
 
 ### Navigation
 
-For this boilerplate, i use React Native Navigation [(RNN)](https://wix.github.io/react-native-navigation/docs/before-you-start/) for navigation and React Material Design [(React Native Paper)](https://callstack.github.io/react-native-paper/).
+For this boilerplate, i use React Native Navigation [(RNN)](https://wix.github.io/react-native-navigation/docs/before-you-start/) for navigation and React Material Design [(React Native Paper)](https://callstack.github.io/react-native-paper/). 
+
+### Utility Style
+A utility style framework for React Native [(OsmiCSX)](https://osmicsx.github.io/docs/).
+
 
--- src/screens/HomeComponents/ListScreens.js
+++ src/screens/HomeComponents/ListScreens.js
@@ -1,17 +1,28 @@
 import React, { useEffect } from 'react'
 import { View, Text } from 'react-native'
+import { List } from 'react-native-paper'
+import { apply } from 'osmicsx'
 
 import { globalStyle } from '../../styles/globalStyles'
 
 const ListScreens = () => {
 
-    useEffect(() =>{
+    useEffect(() => {
         console.log("Runnn!!!!!")
-    },[])
+    }, [])
 
     return (
-        <View style={globalStyle.container}>
-            <Text>Hello</Text>
+        <View style={[globalStyle.container, apply("flex bg-gray-800")]}>
+            <List.Section>
+                <List.Subheader style={apply("text-white")}>Some title</List.Subheader>
+                <List.Item 
+                    title="First Item" 
+                    left={() => <List.Icon color="#fff" icon="folder" />} />
+                <List.Item
+                    title="Second Item"
+                    left={() => <List.Icon color="#fff" icon="folder" />}
+                />
+            </List.Section>
         </View>
     )
 }
